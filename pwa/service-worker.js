2c98ee27143be799271caf7d85c58a3576e74960
smpn9sinjai <157329034+smpn9sinjai@users.noreply.github.com>
2026-01-11 12:47:01 +0800

    Update service-worker.js

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -9,6 +9,6 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 });
